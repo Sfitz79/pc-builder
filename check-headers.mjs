6a7dbdfe1c468482961b3659db32d2db7e5ae054
sfitz79 <simonfitzner@hotmail.com>
2026-08-04 20:39:12 +0100

    chore: remove orphaned legacy scripts, scrapers, and stale data files

--- check-headers.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-const files = fs.readdirSync('src/data').filter(f => f.endsWith('.csv'));
-for (const f of files) {
-  const h = fs.readFileSync('src/data/' + f, 'utf-8').split('\n')[0];
-  console.log(f + ': ' + h);
-}
